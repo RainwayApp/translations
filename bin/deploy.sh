b889e0a85e752341cd050846303528d2b4940961
Evan Banyash <ebanyash@gmail.com>
2019-07-22 13:11:09 -0700

    oops

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-echo "Clearing bucket: s3://your-bucket/path/inside/bucket/if/you/want"
-aws s3 rm s3://s3://rainwayfiles/i18n --recursive --region us-east-1
+echo "Clearing bucket: s3://rainwayfiles/i18n"
+aws s3 rm s3://rainwayfiles/i18n --recursive --region us-east-1
